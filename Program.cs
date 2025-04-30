295d57031d1772ca6aa9793019d20d975777ec7d
RomainHemart <148467078+RomainHemart@users.noreply.github.com>
2025-04-30 15:50:30 +0200

    test

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//sumaray
+// This is just a test
+
+string name = "John Doe";
+Console.WriteLine("Hello, " + name + "!");
