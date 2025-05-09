a2ff1ff5f2c6acc3b4243526d9e8a77aaf7bc033
Alban <godier.alban@gmail.com>
2025-05-09 17:05:44 +0200

    ViewModel and Job interfaces

--- ViewModel.cs
+++ ViewModel.cs
@@ -1,13 +1,31 @@
-﻿using System;
+﻿using EasySave.Model;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EasySave;
 
-public interface IViewModel {
+public interface IViewModel : INotifyPropertyChanged {
+    List<IBackupJob> BackupJobs { get; }
+    List<IBackupJobState> BackupJobStates { get; }
+    ILanguage Language { get; }
+    IConfiguration Configuration { get; }
+    void RunMain();
+    void RunCommandRun();
+    void RunCommandList();
+    void RunCommandAdd();
+    void RunCommandRemove();
+    void RunCommandLanguage();
+    List<string> ParseCommand(string command);
+    void OnLanguageChanged(string language);
+    void OnJobStateChanged(IBackupJobState jobState);
+    void OnConfigurationChanged();
+    void OnPropertyChanged(string propertyName);
 }
 internal class ViewModel {
 }
 
+ 
