40e10b111c13579c8307e77f3e8493cbedbe097e
Beboudu32 <voisinnoah088@gmail.com>
2025-05-12 09:19:17 +0200

    Implémentation BackupJobState, BackupState, StateFile

--- Model/BackupState.cs
+++ Model/BackupState.cs
@@ -16,6 +16,18 @@ namespace EasySave.Model
     }
     public class BackupState 
     {
-
+        public IStateFile File { get; set; }
+        public List<IBackupJobState> JobState { get; set; }
+        public void OnJobStateChanged()
+        {
+            File.Save(JobState);
+        }
+        public IBackupJobState CreateJobState(IBackupJobState backupJobState)
+        {
+            JobState.Add(backupJobState);
+            backupJobState.JobStateChanged += (sender, args) => OnJobStateChanged();
+            return backupJobState;
+        }
+        
     }
 }
--- Model/StateFile.cs
+++ Model/StateFile.cs
@@ -3,9 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.Json;
 
 namespace EasySave.Model
 {
+    public class JobStateDto
+    {
+        public string Name { get; set; }
+        public string SourceFilePath { get; set; }
+        public string TargetFilePath { get; set; }
+        public double TotalFilesToCopy { get; set; }
+        public double TotalFilesSize { get; set; }
+        public double NbFilesLeftToDo { get; set; }
+        public int Progression { get; set; }
+        public string State { get; set; } 
+    }
     public interface IStateFile
     {
         public void Save(List<IBackupJobState> jobsState);
@@ -13,6 +25,58 @@ namespace EasySave.Model
     }
     public class StateFile 
     {
+        public void Save(List<IBackupJobState> jobsState) {
+            var dtoList = new List<JobStateDto>();
+            IBackupJob jobname = new BackupJob();
+            foreach (var jobstate in jobsState)
+            {
+                var dto = new JobStateDto
+                {
+                    Name = jobname.Name,
+                    SourceFilePath = jobstate.SourceFilePath,
+                    TargetFilePath = jobstate.DestinationFilePath,
+                    TotalFilesToCopy = jobstate.TotalFilesToCopy,
+                    TotalFilesSize = jobstate.TotalFilesSize,
+                    NbFilesLeftToDo = jobstate.FilesLeft,
+                    Progression = jobstate.Progression,
+                    State = jobstate.state.ToString(),
 
+                };
+                dtoList.Add(dto);
+            }
+            var jsonString = JsonSerializer.Serialize(dtoList, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText("c:\\temp\\state.json", jsonString);
+        }
+        public List<IBackupJobState> Read()
+        {
+            string jsonString = File.ReadAllText("c:\\temp\\state.json");
+
+            var dtoList = JsonSerializer.Deserialize<List<JobStateDto>>(jsonString);
+            var result = new List<IBackupJobState>();
+
+            foreach (var dto in dtoList)
+            {
+                IBackupJob Job = new BackupJob() {
+                    Name = dto.Name
+            }; 
+                
+                BackupJobState jobState = new BackupJobState(Job)
+                {
+                    SourceFilePath = dto.SourceFilePath,
+                    DestinationFilePath = dto.TargetFilePath,
+                    TotalFilesToCopy = dto.TotalFilesToCopy,
+                    TotalFilesSize = dto.TotalFilesSize,
+                    FilesLeft = dto.NbFilesLeftToDo,
+                    FilesLeftSize = dto.TotalFilesSize - (dto.TotalFilesSize * dto.Progression / 100.0),
+                    Progression = dto.Progression,
+                    State = Enum.Parse<State>(dto.State)
+                };
+
+                result.Add((IBackupJobState)jobState);
+            }
+
+            return result;
+        }
     }
 }
+    
