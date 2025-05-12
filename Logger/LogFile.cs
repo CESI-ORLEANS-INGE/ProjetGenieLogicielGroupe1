d460f90359ae15d4636c1765b851dbb30046a8c7
Cyr DJOKI <djokicyr@gmail.Com>
2025-05-12 09:57:06 +0200

    Ma partie

---
+++ Logger/LogFile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using ILog;
+namespace EasySave.Model;
+
+public interface ILogFile
+{
+    public void Save(ILog Log) { get; }
+    public List <ILog> Read() { get; }
+
+
+}
+public class LogFile { }
