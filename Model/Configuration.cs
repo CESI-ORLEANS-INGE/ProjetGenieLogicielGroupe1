9e25558107b5077f34122d99c89414b95c9b17fc
RomainHemart <148467078+RomainHemart@users.noreply.github.com>
2025-05-15 11:41:06 +0200

    adding comment to the configuration + testing UI responsive

--- Model/Configuration.cs
+++ Model/Configuration.cs
@@ -2,11 +2,19 @@ using System;
 using System.Collections.Generic;
 
 namespace EasySave.Model {
+    /// <summary>
+    /// Event arguments for the ConfigurationChanged event
+    /// </summary>
     public class ConfigurationChangedEventArgs : EventArgs {
+        // Property name that changed
         public string? PropertyName { get; set; }
     }
+    // delegate for the ConfigurationChanged event
     public delegate void ConfigurationChangedEventHandler(object sender, ConfigurationChangedEventArgs e);
 
+    /// <summary>
+    /// Configuration interface
+    /// </summary>
     public interface IConfiguration {
         public const string DEFAULT_LANGUAGE = "FR";
         public const string DEFAULT_STATE_FILE = "state.json";
@@ -22,68 +30,119 @@ namespace EasySave.Model {
     }
 
     public class Configuration : IConfiguration {
+        /// <summary>
+        /// Singleton instance of Configuration
         public static Configuration? Instance { get; private set; }
 
+        // private fields
+        // Language of the application
         private static string? _Language;
+        // State file of the application
         private static string? _StateFile;
+        // List of jobs
         private static List<IBackupJobConfiguration>? _Jobs;
 
+        /// <summary>
+        /// Language of the application
+        /// </summary>
         public string Language {
+            // getting the language default value
             get => _Language ?? IConfiguration.DEFAULT_LANGUAGE;
-            set {
+            // setting the language and raising the event
+            set
+            {
+                // load the language
                 _Language = value;
+                // set the configuration changed event
                 this.ConfigurationChanged?.Invoke(this, new ConfigurationChangedEventArgs {
+                    // set the new language to save
                     PropertyName = nameof(Language)
                 });
             }
         }
+        /// <summary>
+        /// state file of the application
+        /// </summary>
         public string StateFile {
+            // check if the state file is set
+            // if not, throw an exception
             get => _StateFile ?? throw new InvalidOperationException("State file is not set");
-            set {
+            // set the state file and raise the event
+            set
+            {
+                // set the state file
                 _StateFile = value;
+                // set the configuration changed event
                 this.ConfigurationChanged?.Invoke(this, new ConfigurationChangedEventArgs {
+                    // set the new state file to save
                     PropertyName = nameof(StateFile)
                 });
             }
         }
+        /// <summary>
+        /// List of jobs
+        /// </summary>
         public List<IBackupJobConfiguration> Jobs {
+            // getting the jobs list
             get => _Jobs ?? [];
-            set {
+            // setting the jobs list and raising the event
+            set
+            {
                 _Jobs = value;
+                // set the configuration changed event
                 this.ConfigurationChanged?.Invoke(this, new ConfigurationChangedEventArgs {
+                    // set the new jobs list to save
                     PropertyName = nameof(Jobs)
                 });
             }
         }
 
+        /// <summary>
+        /// Method to create a new instance of Configuration
         public Configuration() {
+            // check if the instance is already created
             if (Configuration.Instance != null) {
+                // if the instance is already created, throw an exception
                 throw new InvalidOperationException("Configuration is already initialized.");
             }
-
+            // assign of the values configuration
             Configuration.Instance = this;
         }
 
+        /// <summary>
+        /// Constructor to create a new instance of Configuration
+        /// </summary>
         public Configuration(object configuration) {
+            // check if the instance is already initialized
             if (Configuration.Instance != null) {
+                // if the instance is already created, throw an exception
                 throw new InvalidOperationException("Configuration is already initialized.");
             }
-
+            // assign the values configuration
             if (configuration.GetType().GetProperty("Language") is not null) {
+                // Set the language from the configuration
                 this.Language = configuration.GetType().GetProperty("Language")?.GetValue(configuration)?.ToString() ?? IConfiguration.DEFAULT_LANGUAGE;
             }
+            // Check if the state file is set
             if (configuration.GetType().GetProperty("StateFile") is not null) {
+                // Set the state file from the configuration
                 this.StateFile = configuration.GetType().GetProperty("StateFile")?.GetValue(configuration)?.ToString() ?? IConfiguration.DEFAULT_STATE_FILE;
             }
+            // Check if the jobs list is set
             if (configuration.GetType().GetProperty("Jobs") is not null) {
+                // Set the jobs list from the configuration
                 this.Jobs = configuration.GetType().GetProperty("Jobs")?.GetValue(configuration) as List<IBackupJobConfiguration> ?? [];
             } else {
+                // If the jobs list is not set, initialize it to an empty list
                 this.Jobs = [];
             }
-
+            // Subscribe to the JobConfigurationChanged event for each job
             foreach (IBackupJobConfiguration jobConfiguration in this.Jobs) {
+                // Subscribe to the JobConfigurationChanged event
                 jobConfiguration.JobConfigurationChanged += (sender, args) => {
+                    // Raise the ConfigurationChanged event when a job configuration changes
                     this.ConfigurationChanged?.Invoke(this, new ConfigurationChangedEventArgs {
+                        // Set the property name to Jobs
                         PropertyName = nameof(Jobs)
                     });
                 };
@@ -92,23 +151,41 @@ namespace EasySave.Model {
             Configuration.Instance = this;
         }
 
+        /// <summary>
+        /// add a new job to the configuration
+        /// </summary>
+        /// param name="jobConfiguration"></param>
         public void AddJob(IBackupJobConfiguration jobConfiguration) {
+            // add the job to the list of jobs
             this.Jobs.Add(jobConfiguration);
 
+            // subscribe to the JobConfigurationChanged event
             jobConfiguration.JobConfigurationChanged += (sender, args) => {
+                // raise the ConfigurationChanged event when a job configuration changes
                 this.ConfigurationChanged?.Invoke(this, new ConfigurationChangedEventArgs {
+                    // set the property name to Jobs
                     PropertyName = nameof(Jobs)
                 });
             };
 
+            // raise the ConfigurationChanged event
             this.ConfigurationChanged?.Invoke(this, new ConfigurationChangedEventArgs {
+                // set the property Jobs to save
                 PropertyName = nameof(Jobs)
             });
         }
 
+        /// <summary>
+        /// remove a job from the configuration
+        /// </summary>
+        /// param name="jobConfiguration"></param>
         public void RemoveJob(IBackupJobConfiguration jobConfiguration) {
+            // remove the job from the list of jobs
             this.Jobs.Remove(jobConfiguration);
+            // send the event
+            // raise the ConfigurationChanged event
             this.ConfigurationChanged?.Invoke(this, new ConfigurationChangedEventArgs {
+                // set the property Jobs to save
                 PropertyName = nameof(Jobs)
             });
         }
