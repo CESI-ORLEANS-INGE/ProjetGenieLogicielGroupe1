76a942b5c79d5d5ff619b4026255bf2947113ade
Beboudu32 <voisinnoah088@gmail.com>
2025-05-16 10:07:05 +0200

    Implémentation de Crypto.cs et définition de ProcessesDetector.cs

---
+++ Model/ProcessesDetector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySave.Model
+{
+    public interface IProcessesDetector
+    {
+        public Dictionary<string,bool> Processes { get; }
+        public Task task { get; }
+
+        public bool CheckProcesses();
+
+        public event EventHandler ProcessStarded;
+        public event EventHandler ProcessEnded;
+        public event EventHandler AllProcessEnded;
+        public event EventHandler OneProcessRunning;
+    }
+    public class ProcessesDetector
+    {
+    }
+}
