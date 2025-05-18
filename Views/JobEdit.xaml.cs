049ffb1d64409636801a27e178925d7625e17093
RomainHemart <148467078+RomainHemart@users.noreply.github.com>
2025-05-18 15:45:03 +0200

    starting debug edit

---
+++ Views/JobEdit.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using EasySave.Views;
+
+namespace EasySave.Views
+{
+    public partial class JobEdit : Window
+    {
+        public JobEdit()
+        {
+            InitializeComponent();
+        }
+    }
+}
