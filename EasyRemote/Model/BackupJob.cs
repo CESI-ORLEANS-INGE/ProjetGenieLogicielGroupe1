4c625157948c722f144af7b0c458eff36f9f2500
Beboudu32 <voisinnoah088@gmail.com>
2025-05-28 11:20:03 +0200

    .

---
+++ EasyRemote/Model/BackupJob.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyRemote
+{
+    public interface IBackupJob
+    {
+        public string Name { get; set; }
+        IDirectory Source { get; set; }
+        IDirectory Destination { get; set; }
+    }
+    class BackupJob
+    {
+        public string Name { get; set; }
+        IDirectory Source { get; set; }
+        IDirectory Destination { get; set; }
+    }
+}
---
+++ EasyRemote/Model/BackupJobState.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyRemote
+{ 
+    public interface IBackupJobState
+{
+    IBackupJob BackupJob { get; set; }
+    public string SourceFilePath { get; set; }
+    public string DestinationFilePath { get; set; }
+    State State { get; set; }
+    public double TotalFilesToCopy { get; set; }
+    public double TotalFileSize { get; set; }
+    int Progression { get; set; }
+
+}
+
+    class BackupJobState
+    {
+        IBackupJob BackupJob { get; set; }
+        public string SourceFilePath { get; set; }
+        public string DestinationFilePath { get; set; }
+        State State { get; set; }
+        public double TotalFilesToCopy { get; set; }
+        public double TotalFileSize { get; set; }
+        int Progression { get; set; }
+    }
+}
