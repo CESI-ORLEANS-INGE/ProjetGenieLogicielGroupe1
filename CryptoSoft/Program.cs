3caa52911da505a71d8156133508e0d985fd8aee
Alban <godier.alban@gmail.com>
2025-06-02 11:24:02 +0200

    AHHHHHHHHHHH FINIIIIIIIIII !!!

--- CryptoSoft/Program.cs
+++ CryptoSoft/Program.cs
@@ -28,9 +28,9 @@ public static class Program {
                         Console.WriteLine("Connection closed");
                     }
                 }
-                if (pipes.Count == 0) {
-                    Environment.Exit(0); // Exit if no pipes are connected
-                }
+                //if (pipes.Count == 0) {
+                //    Environment.Exit(0); // Exit if no pipes are connected
+                //}
                 Thread.Sleep(1000);
             }
         });
@@ -50,39 +50,41 @@ public static class Program {
     }
 
     private static void _HandleConnection(NamedPipeServerStream pipeStream) {
-        using StreamReader reader = new(pipeStream);
-        using StreamWriter writer = new(pipeStream) { AutoFlush = true };
+        try {
+            using StreamReader reader = new(pipeStream);
+            using StreamWriter writer = new(pipeStream) { AutoFlush = true };
 
-        while (pipeStream.IsConnected) {
-            try {
-                string? command = reader.ReadLine();
-                if (command is not null) {
-                    Console.WriteLine($"Received command: {command}");
-                    JsonObject json = JsonNode.Parse(command) as JsonObject ?? throw new InvalidOperationException("Invalid JSON command received.");
-                    string fileName = json["FileName"]?.ToString() ?? throw new InvalidOperationException("File name is required.");
-                    string key = json["CryptoKey"]?.ToString() ?? throw new InvalidOperationException("Key is required.");
+            while (pipeStream.IsConnected) {
+                try {
+                    string? command = reader.ReadLine();
+                    if (command is not null) {
+                        Console.WriteLine($"Received command: {command}");
+                        JsonObject json = JsonNode.Parse(command) as JsonObject ?? throw new InvalidOperationException("Invalid JSON command received.");
+                        string fileName = json["FileName"]?.ToString() ?? throw new InvalidOperationException("File name is required.");
+                        string key = json["CryptoKey"]?.ToString() ?? throw new InvalidOperationException("Key is required.");
 
-                    if (File.Exists(fileName)) {
-                        FileManager file = new(fileName, key);
-                        double duration = file.TransformFile();
+                        if (File.Exists(fileName)) {
+                            FileManager file = new(fileName, key);
+                            double duration = file.TransformFile();
 
-                        if (duration >= 0) {
-                            Console.WriteLine($"File '{fileName}' processed successfully in {duration} ms.");
+                            if (duration >= 0) {
+                                Console.WriteLine($"File '{fileName}' processed successfully in {duration} ms.");
+                            } else {
+                                Console.WriteLine($"Error processing file '{fileName}'.");
+                            }
+
+                            writer.WriteLine(new JsonObject {
+                                ["FileName"] = fileName,
+                                ["Duration"] = duration
+                            }.ToJsonString());
                         } else {
-                            Console.WriteLine($"Error processing file '{fileName}'.");
+                            Console.WriteLine($"Error: File '{fileName}' does not exist.");
                         }
-
-                        writer.WriteLine(new JsonObject {
-                            ["FileName"] = fileName,
-                            ["Duration"] = duration
-                        }.ToJsonString());
-                    } else {
-                        Console.WriteLine($"Error: File '{fileName}' does not exist.");
                     }
+                } catch (Exception ex) {
+                    Console.WriteLine($"Error processing connection: {ex.Message}");
                 }
-            } catch (Exception ex) {
-                Console.WriteLine($"Error processing connection: {ex.Message}");
             }
-        }
+        } catch { }
     }
 }
